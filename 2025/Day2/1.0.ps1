5be196fee5b64b265afe78d5a4607dd43b502036
MariaLysik <ml.medius@gmail.com>
2025-12-03 02:27:39 +0100

    Day2

---
+++ 2025/Day2/1.0.ps1
@@ -0,0 +1,29 @@
+﻿$sumOfInvalidIds = 0
+
+function Is-Invalid([string]$number) {
+  if ($number.Length -band 1) { # check if odd number
+    return $false
+  }
+  $halfDigits = $number.Length / 2
+  return $number.substring(0,$halfDigits) -eq $number.substring($halfDigits)
+}
+
+$ranges = (Get-Content .\2025\Day2\1.0.txt) -split ','
+$ranges | ForEach-Object {
+  Write-Host $_
+  $min, $max = $_ -split '-'
+  $minDigits = $min.Length
+  $maxDigits = $max.Length
+  if ($minDigits -eq $maxDigits -and $minDigits%2) {
+    Write-Host 'min and max has the same and odd number of digits, skipping'
+    return
+  }
+  for ($i = [uint64]$min; $i -le [uint64]$max; $i++) {
+    if (Is-Invalid "$i") {
+      Write-Host 'found invalid' $i
+      $sumOfInvalidIds += $i
+    }
+  }
+}
+
+Write-Host 'part 1:' $sumOfInvalidIds
---
+++ 2025/Day2/1.2.ps1
@@ -0,0 +1,45 @@
+﻿$sumOfInvalidIds = 0
+
+function Is-Invalid([string]$number) {
+  $length = $number.Length
+  if ($length -le 1) {
+    return $false
+  }
+  if ($number -match "^(.)\1*$") {
+    return $true
+  }
+  for ($i = 2; $i -le $length / 2; $i++) {
+    if ($length % $i -ne 0) {
+      #Write-Host 'length' $length 'not a multiplication of' $i '> skipping'
+      continue
+    }
+    $chunkSize = $length / $i
+    $expectedPattern = $number.substring(0, $chunkSize)
+    #Write-Host 'chunk size' $chunkSize 'expected pattern' $expectedPattern
+    $found = $true
+    for ($j = 1; $j -lt $i; $j++) {
+      if ($number.substring($j * $chunkSize, $chunkSize) -ne $expectedPattern) {
+        $found = $false
+        break
+      }
+    }
+    if ($found) {
+      return $true
+    }
+  }
+  return $false
+}
+
+$ranges = (Get-Content .\2025\Day2\1.0.txt) -split ','
+$ranges | ForEach-Object {
+  #Write-Host $_
+  $min, $max = $_ -split '-'
+  for ($i = [uint64]$min; $i -le [uint64]$max; $i++) {
+    if (Is-Invalid "$i") {
+      #Write-Host 'found invalid' $i
+      $sumOfInvalidIds += $i
+    }
+  }
+}
+
+Write-Host 'part 2:' $sumOfInvalidIds
