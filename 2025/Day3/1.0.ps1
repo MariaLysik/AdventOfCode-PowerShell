5e903f2b268bbb11f413371b07e7f7826928c9af
MariaLysik <ml.medius@gmail.com>
2025-12-03 14:13:24 +0100

    Day 3 Part 1

---
+++ 2025/Day3/1.0.ps1
@@ -0,0 +1,32 @@
+﻿$totalJoltage = 0
+$CACHE = @{}
+
+function Get-MaxJoltage([string]$bank) {
+  if ($CACHE[$bank]) {
+    return $CACHE[$bank]
+  }
+  $arr = $bank.ToCharArray()
+  $firstPointer = 0
+  $secondPointer = 1
+  for ($i = 1; $i -le $arr.Length-2; $i++) {
+    if($arr[$i] -gt $arr[$firstPointer]) {
+      $firstPointer = $i
+      $secondPointer = $i+1
+    }
+    if($secondPointer -lt $i -and $arr[$i] -gt $arr[$secondPointer]) {
+      $secondPointer = $i
+    }
+  }
+  if($secondPointer -lt $arr.Length-1 -and $arr[$arr.Length-1] -gt $arr[$secondPointer]) {
+    $secondPointer = $arr.Length-1
+  }
+  $joltage = $arr[$firstPointer] + $arr[$secondPointer]
+  $CACHE[$bank] = $joltage
+  return $joltage
+}
+
+Get-Content .\2025\Day3\1.1.txt | ForEach-Object {
+  $totalJoltage += [int](Get-MaxJoltage $_)
+}
+
+Write-Host 'Part 1:' $totalJoltage
