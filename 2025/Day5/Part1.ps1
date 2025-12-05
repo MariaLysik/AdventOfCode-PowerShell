9223b161a4e2fc21a5ed3b64b6f734d4c94618b2
MariaLysik <ml.medius@gmail.com>
2025-12-05 20:25:45 +0100

    Day 5 Part 2

---
+++ 2025/Day5/Part1.ps1
@@ -0,0 +1,47 @@
+﻿$f = Get-Content .\2025\Day5\1.1.txt
+
+class Interval {
+  [UInt64]$Min
+  [UInt64]$Max
+  [Interval]$Next
+  [Interval]$Prev
+
+  Interval([UInt64]$start,[UInt64]$end) {
+    if ($start -gt $end) {
+      throw "Invalid interval!"
+    }
+    $this.Min = $start
+    $this.Max = $end
+    $this.Next = $null # Initialize Next to null
+    $this.Prev = $null # Initialize Prev to null
+  }
+
+  Display() {
+    Write-Host $this.Min ":" $this.Max
+  }
+}
+
+$products = @()
+$intervals = @()
+foreach ($line in $f) {
+  if ($line -and $line.Contains('-')) {
+    $start, $end = $line.Split('-')
+    $intervals += [Interval]::new($start, $end)
+  }
+  elseif ($line) {
+    $products += [UInt64]$line
+  }
+}
+
+$freshCount = 0
+# primitive solution > change it later to LinkedList one with merging intervals on a fly
+foreach($product in $products) {
+  foreach($interval in $intervals) {
+    if ($product -ge $interval.Min -and $product -le $interval.Max) {
+      $freshCount++
+      break
+    }
+  }
+}
+
+Write-Host $freshCount
---
+++ 2025/Day5/Part2.ps1
@@ -0,0 +1,49 @@
+﻿$f = Get-Content .\2025\Day5\1.1.txt
+
+$RANGES = @{}
+
+foreach ($line in $f) {
+  if(-not $line) {
+    break
+  }
+  if ($line -and $line.Contains('-')) {
+    $start, $end = $line.Split('-')
+    $start = [UInt64]$start
+    $end   = [UInt64]$end
+    if ($RANGES[$start]) {
+      $RANGES[$start] = [Math]::Max($end, $RANGES[$start])
+    }
+    else {
+      $RANGES[$start] = $end
+    }
+  }
+}
+
+$totalFresh = 0
+if ($RANGES.Keys.Count) {
+  $keys = @($RANGES.Keys | Sort-Object)
+  $min = $null
+  $max = $null
+  foreach ($key in $keys) {
+    if ($null -eq $min) {
+      $min = $key
+      $max = $RANGES[$key]
+      continue
+    }
+    if ($max -ge $key) {
+      Write-Host 'merging' $min $max 'with' $key $RANGES[$key]
+      $max = [Math]::Max($max, $RANGES[$key])
+      continue
+    }
+    $freshInRange = $max - $min + 1
+    Write-Host 'fresh in'  $min $max $freshInRange
+    $totalFresh += $freshInRange
+    $min = $key
+    $max = $RANGES[$key]
+  }
+  $freshInRange = $max - $min + 1
+  Write-Host 'fresh in'  $min $max $freshInRange
+  $totalFresh += $freshInRange
+}
+
+Write-Host 'Part 2:' $totalFresh
