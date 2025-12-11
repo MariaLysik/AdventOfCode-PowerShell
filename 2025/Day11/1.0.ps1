a4b5998d0fbc4c70803d6d63e1b96d9beae4559f
MariaLysik <ml.medius@gmail.com>
2025-12-11 23:49:13 +0100

    Day11 Part1

---
+++ 2025/Day11/1.0.ps1
@@ -0,0 +1,32 @@
+﻿Measure-Command {
+  $f = Get-Content .\2025\Day11\1.0.txt
+
+  $start = 'you'
+  $end = 'out'
+  $CACHE = @{}
+  $GRAPH = @{}
+  
+  function To-End([string]$from) {
+    if($CACHE[$from]) {
+      return $CACHE[$from]
+    }
+    if ($from -eq $end) {
+      return 1
+    }
+    $paths = 0
+    foreach ($node in $GRAPH[$from]) {
+      $paths += (To-End -from $node)
+    }
+    $CACHE[$from] = $paths
+    return $paths
+  }
+
+  foreach ($line in $f) {
+    $current, $followers = $line -split ':'
+    $GRAPH[$current] = $followers.trim().split()
+  }
+
+  $pathCount = (To-End -from $start)
+
+  Write-Host "Part 1:" $pathCount
+}
